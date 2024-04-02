3bcbbdc135331f1e12f5cf43ecfa5ab240f21834
iankristoper <iankristoper26@gmail.com>
2024-04-02 12:03:04 +0800

    UPDATE: sub class features

--- src/main/java/com/ian/banking_application/IndividualAccount.java
+++ src/main/java/com/ian/banking_application/IndividualAccount.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.ian.banking_application;
 
 /**
  *
  * @author Ian Tagano
+ * 
  */
-public class IndividualAccount {
+public class IndividualAccount extends Account
+{
+    //constructor for the individual account to initialize the account number
+    public IndividualAccount(String accountNumber)
+    {
+        super(accountNumber);
+    }
     
+    @Override 
+    public void withdraw(double amount)
+    {
+        //check if there is enough balane 
+        if(amount <= balance)
+        {
+            balance = balance - amount;
+            System.out.println("Withdrawal of $" + amount + " successful. Current balance: $" + balance);
+        }
+        else 
+        {
+            System.out.println("Insufficient funds. Withdrawal not allowed.");
+        }
+    }
+        
 }
