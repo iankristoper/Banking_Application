2953a1fb61b72f85801e89dc6fd4b0b385901bd8
iankristoper <iankristoper26@gmail.com>
2024-04-02 10:49:12 +0800

    UPDATE: main class features

---
+++ src/main/java/com/ian/banking_application/Banking_Application.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ian.banking_application;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+public class Banking_Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
