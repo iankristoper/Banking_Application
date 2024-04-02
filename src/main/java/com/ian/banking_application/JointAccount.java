f60099b37b9e0a81dfb1b607f238f4a1c88bdff7
iankristoper <iankristoper26@gmail.com>
2024-04-02 12:03:10 +0800

    UPDATE: sub class features

--- src/main/java/com/ian/banking_application/JointAccount.java
+++ src/main/java/com/ian/banking_application/JointAccount.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.ian.banking_application;
 
 /**
  *
  * @author Ian Tagano
  */
-public class JointAccount {
+
+public class JointAccount extends Account
+{
+    //constructor for the joint account to initializw account number 
+    public JointAccount(String accountNumber)
+    {
+        super(accountNumber);
+    }
     
+    
+    @Override
+    public void withdraw(double amount) 
+    {
+        //check if there is enough balance 
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal of $" + amount + " successful. Current balance: $" + balance);
+        } 
+        else 
+        {
+            System.out.println("Insufficient funds. Withdrawal not allowed.");
+        }
+    }
 }
