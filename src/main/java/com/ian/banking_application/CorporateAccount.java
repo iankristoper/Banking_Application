7b206449b72da52d23589c42a9eaae8ea769b077
iankristoper <iankristoper26@gmail.com>
2024-04-02 12:03:15 +0800

    UPDATE: sub class features

--- src/main/java/com/ian/banking_application/CorporateAccount.java
+++ src/main/java/com/ian/banking_application/CorporateAccount.java
@@ -5,6 +5,28 @@ package com.ian.banking_application;
  *
  * @author Ian Tagano
  */
-public class CorporateAccount {
+public class CorporateAccount extends Account
+{
+    //constructor for corporate account to initialize account number 
+    public CorporateAccount(String accountNumber) 
+    {
+        super(accountNumber);
+    }
+
     
+    //override wintdraw methods 
+    @Override
+    public void withdraw(double amount) 
+    {
+        //check if there is enough balance 
+        if (amount <= balance) 
+        {
+            balance -= amount;
+            System.out.println("Withdrawal of $" + amount + " successful. Current balance: $" + balance);
+        } 
+        else 
+        {
+            System.out.println("Insufficient funds. Withdrawal not allowed.");
+        }
+    }
 }
