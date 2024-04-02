bbd47b817e0a9781bc9459ffbd2926c4fc322364
iankristoper <iankristoper26@gmail.com>
2024-04-02 12:02:51 +0800

    UPDATE: main class features

--- src/main/java/com/ian/banking_application/Banking_Application.java
+++ src/main/java/com/ian/banking_application/Banking_Application.java
@@ -1,16 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.ian.banking_application;
 
 /**
  *
  * @author Ian Tagano
  */
-public class Banking_Application {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Banking_Application 
+{
+
+    public static void main(String[] args) 
+    {
+        Account individual = new IndividualAccount("A1200");
+        Account joint = new JointAccount("A1300");
+        Account corporate = new CorporateAccount("A1400");
+        
+        individual.deposit(100000);
+        individual.withdraw(1000);
+        
+        joint.deposit(2000);
+        joint.withdraw(100);
+        
+        corporate.deposit(1000000);
+        corporate.withdraw(20000);
     }
 }
